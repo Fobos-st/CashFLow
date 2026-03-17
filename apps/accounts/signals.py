c26f6f785f70210939fec8bfd1045a72217f3aa2
Amarsana <fipikmen3@gmail.com>
2026-03-17 15:16:40 +0800

    add(signals): Добавил сигнал, который создает счет и остальное
    Добавил функции, которая при создании нового CustomUser, сразу создавал для него счёт, статусы, типы, категории и подкатегории

---
+++ apps/accounts/signals.py
@@ -0,0 +1,34 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from apps.accounting.personal.models import (PersonalAbstractAccount, PersonalTransactionType,
+                                             PersonalTransactionStatus, PersonalCategory)
+from apps.accounts.models import CustomUser
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        PersonalAbstractAccount.objects.create(user=instance, currency="RUB")
+        PersonalTransactionStatus.objects.create(user=instance, title="Бизнес", is_system=True)
+        PersonalTransactionStatus.objects.create(user=instance, title="Личное", is_system=True)
+        PersonalTransactionStatus.objects.create(user=instance, title="Налог", is_system=True)
+
+        withdrawals = PersonalTransactionType.objects.create(user=instance, title="Списание", is_system=True)
+        category = PersonalCategory.objects.create(user=instance, type_transaction=withdrawals, is_system=True, title="Инфраструктура")
+        PersonalCategory.objects.create(user=instance, type_transaction=withdrawals, parent=category,
+                                        is_system=True, title="VPS")
+        PersonalCategory.objects.create(user=instance, type_transaction=withdrawals, parent=category,
+                                        is_system=True, title="Proxy")
+        category = PersonalCategory.objects.create(user=instance, type_transaction=withdrawals, is_system=True, title="Маркетинг")
+        PersonalCategory.objects.create(user=instance, type_transaction=withdrawals, parent=category,
+                                        is_system=True, title="Avito")
+        PersonalCategory.objects.create(user=instance, type_transaction=withdrawals, parent=category,
+                                        is_system=True, title="Farpost")
+
+        addition = PersonalTransactionType.objects.create(user=instance, title="Пополнение", is_system=True)
+        category = PersonalCategory.objects.create(user=instance, type_transaction=addition, is_system=True,
+                                                   title="Зарплата")
+        PersonalCategory.objects.create(user=instance, type_transaction=addition, parent=category,
+                                        is_system=True, title="Основная")
+        PersonalCategory.objects.create(user=instance, type_transaction=addition, parent=category,
+                                        is_system=True, title="Аванс")
