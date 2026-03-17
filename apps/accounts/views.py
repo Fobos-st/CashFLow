b583000c2915e904c136f727ae4d2d812ff8cceb
Amarsana <fipikmen3@gmail.com>
2026-03-17 13:05:00 +0800

    add(views.py) представления входа и регистрации
    RegisterView:
    Класс наследуется от View. Реализует регистрацию пользователей с использованием формы CustomUserCreationForm. Представление принимает 2 метода GET и POST.
    GET перенаправляет аутентифицированных юзеров на счёт, для остальных возвращает страницу регистрацией
    POST принимает форму регистрации пользователя, регистрирует и логинит его с сохранением сессии на 2недели, потом редиректит на счёт
    
    LoginView:
    Класс наследуется от View. Реализует вход пользователей в аккаунт с использованием формы CustomUserLoginForm. Представление принимает 2 метода GET и POST
    GET перенаправляет аутентифицированных юзеров на счёт, для остальных возвращает страницу для входа в аккаунт
    POST принимает форму входа пользователя, логинит его с сохранением сессии на 2недели

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+from apps.accounts.forms import CustomUserCreationForm, CustomUserLoginForm
+
+
+class RegisterView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('Testik:page1')
+        form = CustomUserCreationForm()
+        return render(request, 'register.html', {'form': form})
+
+    def post(self, request):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            request.session.set_expiry(1209600)
+            return redirect('Testik:page1')
+        else:
+            form = CustomUserCreationForm()
+        return render(request, 'register.html', {'form': form})
+
+
+class LoginView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('Testik:page1')
+        form = CustomUserLoginForm()
+        return render(request, 'login.html', {'form': form})
+
+
+    def post(self, request):
+        form = CustomUserLoginForm(request=request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            request.session.set_expiry(1209600)
+            return redirect('Tes:profile')
+        else:
+            print(form.errors)
+            form = CustomUserLoginForm()
+        return render(request, 'login.html', {'form': form})
