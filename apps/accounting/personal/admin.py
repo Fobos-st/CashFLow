f19213230efecb510a3023e7b2d5eb89ee5e3ee5
Amarsana <fipikmen3@gmail.com>
2026-03-17 15:12:47 +0800

    add(admin): Добавил новые объекты моделей
    Добавил в админку модели БД, а именно PersonalAbstractAccount, PersonalTransactionStatus, PersonalTransactionType

--- apps/accounting/personal/admin.py
+++ apps/accounting/personal/admin.py
@@ -1,6 +1,6 @@
 from django_mptt_admin.admin import DjangoMpttAdmin
 from django.contrib import admin
-from .models import PersonalCategory
+from .models import PersonalCategory, PersonalAbstractAccount, PersonalTransactionStatus, PersonalTransactionType
 
 
 @admin.register(PersonalCategory)
@@ -9,3 +9,7 @@ class CategoryAdmin(DjangoMpttAdmin):
     Админ-панель модели категорий
     """
     pass
+
+admin.site.register(PersonalTransactionStatus)
+admin.site.register(PersonalAbstractAccount)
+admin.site.register(PersonalTransactionType)
