1d980ec22fe6aebbdeeba847d94b184595a727bf
Amarsana <fipikmen3@gmail.com>
2026-03-19 17:19:16 +0800

    add(core/urls): Глобальная маршрутизация

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.accounting.personal.urls'), name='test'),
+    path('account/', include('apps.accounts.urls'), name='user'),
+    path('create/', include('apps.accounting.create_account.urls'))
 ]
